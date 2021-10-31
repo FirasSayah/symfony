01694e8a83993493c45f090147039c2bdeef3612
Firas Sayah <firassayah5@gmail.com>
2021-10-31 23:29:35 +0100

    CRUD Part

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\PostFormType;
+
 
 /**
      * @Route("/post", name="post.")
@@ -16,33 +18,63 @@ class PostController extends AbstractController
 {
     /**
      * @Route("/", name="post")
+     * @param PostRepository $postRepository
+     * @param  Response
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository)
     {
+        $posts = $postRepository->findAll();
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'postes' => $posts,
         ]);
     }
      /**
      * @Route("/create", name="create")
-     * param Request $request
+     * @param Request $request
      */
 
     public function create(Request $request){
 
         $post = new Post();
-        $post->setTitle('this a title');
+        
+        $form = $this->createForm(PostFormType::class, $post);
 
         $em = $this->getDoctrine()->getManager();
-        $em->persist($post);
-        $em->flush();
+       // $em->persist($post);
+        //$em->flush();
 
         
 
-        return $this->render('post/post.html.twig',
-    [
-        'post'=>$post
-    ]);
+       return $this->render('post/create.html.twig',
+    ['form'=> $form->createView()]);
 
     }
+    /**
+     * @Route("/show/{id}", name="show")
+     * @param Request $request
+     */
+
+    public function show($id,PostRepository $postRepository){
+            $post = $postRepository->find($id);
+
+
+        return $this->render('post/show.html.twig',
+        ['post'=>$post]);
+    }
+     /**
+     * @Route("/remove/{id}", name="remove")
+     * @param Request $request
+     */
+
+     public function remove($id,PostRepository $postRepository):Response 
+
+     {
+        $em = $this->getDoctrine()->getManager();
+        $post = $postRepository->findOneById($id);
+        $em->remove($post);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('post.post'));
+     }
+ 
 }
